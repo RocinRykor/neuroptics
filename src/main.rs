c088109ca92a4776f45843a2832ecc6c95590353
Steven <Briggs.steven369@gmail.com>
2026-07-01 08:47:35 -0700

    Re organize

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Neuroptics: Project Cephalon: Learning Center");
-    println!("Under Construction");
+    let mut args: Vec<String> = env::args().skip(1).collect();
+
+    if args.is_empty() {
+        eprintln!("usage: playground <subcommand> [args...]");
+        return;
+    }
+
+    let subcommand = args.remove(0);
+    // let flags = extract_flags(&mut args);
 }
